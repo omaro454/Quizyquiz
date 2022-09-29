32263f3f47cb4a3728eb4668c3287ecd1ed228ca
Omar <omar.o454@gmail.com>
2022-09-29 00:30:51 -0600

    Added js and question array

---
+++ Assets/js/questions.js
@@ -0,0 +1,67 @@
+// list of all questions, choices, and answers
+var questions = [
+    {
+      title: "Commonly used data types include:",
+      choices: [
+        "Strings",
+      "Booleans",
+      "Numbers",
+      "All of the above" ],
+      answer: "All of the above"
+    },
+    {
+      title: "What is the data type of variables in JavaScript?",
+      choices: [
+        "Object data types",
+        "Function data types",
+        "Number data types",
+        "None of the above"
+      ],
+      answer: "Object data types"
+    },
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    },
+    {
+      title: "Arrays in JavaScript can be used to store ____.",
+      choices: [
+        "Numbers and strings",
+        "Other arrays",
+        "Booleans",
+        "All of the above"
+      ],
+      answer: "All of the above"
+    },
+    {
+      title:
+        "String values must be enclosed within ____ when being assigned to variables.",
+      choices: [
+      "Commas",
+      "Curly brackets",
+      "Quotes",
+      "Parentheses"
+    ],
+      answer: "Quotes"
+    },
+    {
+      title:
+        "A very useful tool used during development and debugging for printing content to the debugger is:",
+      choices: [
+      "JavaScript",
+      "terminal / bash",
+      "for loops",
+      "console.log"],
+      answer: "console.log"
+    },
+    {
+      title: "What is the type of Pop up boxes available in JavaScript?:",
+      choices: ["Alert",
+      "Confirm",
+      "Prompt",
+      "All the above"],
+      answer: "All the above"
+    }
+  ];
+  
---
+++ Assets/js/script.js
@@ -0,0 +1,30 @@
+var questionsEl = document.querySelector("#questions");
+var timerEl = document.querySelector("#time");
+var choicesEl = document.querySelector("#choices");
+var submitBtn = document.querySelector("#submit");
+var startBtn = document.querySelector("#start");
+var initialsEl = document.querySelector("#initials");
+var feedbackEl = document.querySelector("#feedback");
+ 
+var currentQuestionIndex = 0;
+var time = questions.length * 15;
+var timerId;
+ 
+function startQuiz() {
+  // hide start screen
+  var startScreenEl = document.getElementById("start-screen");
+  startScreenEl.setAttribute("class", "hide");
+ 
+  // un-hide questions section
+  questionsEl.removeAttribute("class");
+ 
+  // start timer
+  timerId = setInterval(clockTick, 1000);
+ 
+  // show starting time
+  timerEl.textContent = time;
+ 
+  getQuestion();
+}
+ 
+function getQuestion() 
